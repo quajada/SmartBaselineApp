3fa2e61202543c5d7715a38714e827e351a55f44
Bruno Tabet <tabetbruno@gmail.com>
2022-10-06 16:19:30 +0400

    nb21

--- pages/7_M&V_report.py
+++ pages/7_M&V_report.py
@@ -816,7 +816,7 @@ if st.session_state['M&V'] == 1.3:
 
         # Save the file
 
-        doc.save('M%VReport.docx')
+        doc.save(r"C:\Users\Bruno Tabet\Documents\ENOVA\MVP\THISISATEST.xlsx")
         
         st.session_state['M&V'] = 2
         
