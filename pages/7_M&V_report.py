e05a55e6d525594206af9c5f6d34877aba14ed50
Bruno Tabet <tabetbruno@gmail.com>
2022-10-11 18:18:17 +0400

    n20

--- pages/7_M&V_report.py
+++ pages/7_M&V_report.py
@@ -902,7 +902,7 @@ if st.session_state['M&V'] == 1.3:
         pdf.cell(w = 75.0, h = 5.0, align = "L", txt = "This is my sample text")
         
         oflnme = "Final_report.pdf"
-        b64 = base64.b64encode(pdf.output("",dest="S")).decode()
+        b64 = base64.b64encode(pdf.output(dest='S')).decode()
         st.download_button("Download Report", data=b64, file_name=oflnme, mime="application/octet-stream", help=f"Download file {oflnme}")
         
         #alternatively, if I replace the above last 2 lines with the following 3 lines of code for a download hyperlink, it works fine.
