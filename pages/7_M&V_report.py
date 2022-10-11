151a11c23f12c139087a0e3266c31c825260ae90
Bruno Tabet <tabetbruno@gmail.com>
2022-10-11 18:09:10 +0400

    n18

--- pages/7_M&V_report.py
+++ pages/7_M&V_report.py
@@ -888,7 +888,29 @@ if st.session_state['M&V'] == 1.3:
             file_name="ok.pdf",
             mime="application/octet-stream"
         )
-                
+        
+        
+        from fpdf import FPDF
+        import base64
+        
+        pdf = FPDF() #pdf object
+        pdf=FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
+        
+        pdf.set_font("Times", "B", 18)
+        pdf.set_xy(10.0, 20)
+        pdf.cell(w = 75.0, h = 5.0, align = "L", txt = "This is my sample text")
+        
+        oflnme = "Final_report.pdf"
+        b64 = base64.b64encode(pdf.output("",dest="S")).decode()
+        st.download_button("Download Report", data=b64, file_name=oflnme, mime="application/octet-stream", help=f"Download file {oflnme}")
+        
+        #alternatively, if I replace the above last 2 lines with the following 3 lines of code for a download hyperlink, it works fine.
+        
+        # b64 = base64.b64encode(pdf.output(dest="S"))
+        # html = f'Download file'
+        # st.markdown(html, unsafe_allow_html=True)
+        
         
         st.session_state['M&V'] = 2
         
